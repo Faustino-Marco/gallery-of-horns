34e074bcb0d72fb55136fe8aed76111e699d1c7b
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-06-14 10:40:45 -0700

    started working through lab 01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 import './App.css';
+
+class App extends React.Component{
+
+  render() {
+    return(
+      <Header />
+      <Main />
+      <Footer />
+    )
+  }
+}
 
 function App() {
   return (
---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import react from 'react';
+
+import App from './App';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <HornedBeast
+          title="Uniwhal"
+          imageUrl="#"
+      </main>
+    )
+  }
+}
