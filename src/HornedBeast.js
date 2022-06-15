8a1a32f67cdbf47851b626926232cbbe5f4e5d1f
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-06-14 23:49:50 -0700

    final commit tonight for lab 01

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 class HornedBeast extends React.Component {
   render() {
-    return(
+    return (
     <article>
       <h2>{this.props.title}</h2>
       <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
