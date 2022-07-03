6942a6272704f84f7aa9ad31acdf73d76d5792ea
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-07-03 14:58:31 -0700

    lab 4 complete

--- src/App.js
+++ src/App.js
@@ -19,16 +19,6 @@ class App extends React.Component {
     }
   }
 
-  // addHearts = () => {
-  //   this.setState({
-  //     heart: this.state.heart + 'heart emoji'
-  //   })
-  // }
-
-  resultBeasts = (newBeasts) => {
-    this.setState({allBeasts: newBeasts});
-  }
-
   handleOnHide = () => {
     this.setState({
       showModal: false,
@@ -45,13 +35,68 @@ class App extends React.Component {
     });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let username = event.target.name.value;
+    let horns = event.target.selected.value;
+    console.log(`username: ${username}`);
+    console.log(`horns: ${horns}`);
+    this.setState({
+      howToSort: horns,
+      name: username
+    });
+    console.log(`horns: ${horns}`);
+  }
+
+  handleHorn = (numHorns) => {
+    console.log(numHorns);
+    if (numHorns == "All") {
+      this.props.searchedHorn(data);
+    } else if (numHorns === "1") {
+      console.log(data.filter(beast => beast.horns == 1));
+      let hornCriteria = data.filter(beast =>
+        beast.horns == 1);
+      this.resultBeasts(hornCriteria);
+    } else if (numHorns === "2") {
+      console.log(data.filter(beast => beast.horns == 2));
+      let hornCriteria = data.filter(beast =>
+        beast.horns == 2);
+      this.resultBeasts(hornCriteria);
+    } else if (numHorns === "3") {
+      console.log(data.filter(beast => beast.horns == 3));
+      let hornCriteria = data.filter(beast =>
+        beast.horns == 3);
+      this.resultBeasts(hornCriteria);
+    } else {
+      console.log(data.filter(beast => beast.horns == 100));
+      let hornCriteria = data.filter(beast =>
+        beast.horns == 100);
+      this.resultBeasts(hornCriteria);
+    }
+  };
+
+  handleChange = event => {
+    this.setState({
+      chosenHorn: event.target.value
+    });
+    this.handleHorn(event.target.value);
+  }
+
+  resultBeasts = (newBeasts) => {
+    this.setState({allBeasts: newBeasts});
+  };
+
   render() {
     return (
       <>
         <Header />
         <Main
-          data={data}
-          handleOnShowModal={this.handleOnShowModal} />
+          data={this.state.allBeasts}
+          handleOnShowModal={this.handleOnShowModal}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          // handleHorn={this.handleHorn} 
+          />
         <Footer />
         <Modal
           show={this.state.showModal}
---
+++ src/BeastForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FormGroup } from 'react-bootstrap';
+// import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import data from './data.json';
+// import HornedBeast from './HornedBeast';
+
+let searchArr = [1, 2, 3, 100];
+
+class BeastForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      horns: '',
+      searchedHorn: '',
+      chosenHorn: searchArr,
+      sortedData: data,
+    };
+  };
+
+  render() {
+    return(
+      <>
+      <form onSubmit={this.props.handleSubmit}>
+        <label>Name:
+          <input type="text" name="name"/>
+        </label>
+        <fieldset>
+          <legend>Sort by Number of Horns</legend>
+          <select name="selected" onChange={this.props.handleChange}>
+          <option value="All">All</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="100">100</option>
+          </select>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+      </>
+    );
+  };
+
+};
+
+export default BeastForm;
