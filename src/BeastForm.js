15a9434d108921198953a220b7a334a652af2e98
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-07-04 23:17:06 -0700

    changed to React-bootstrap form

--- src/BeastForm.js
+++ src/BeastForm.js
@@ -19,24 +19,34 @@ class BeastForm extends React.Component {
   };
 
   render() {
-    return(
+    return (
       <>
-      <form onSubmit={this.props.handleSubmit}>
-        <label>Name:
-          <input type="text" name="name"/>
-        </label>
-        <fieldset>
-          <legend>Sort by Number of Horns</legend>
-          <select name="selected" onChange={this.props.handleChange}>
+        <Form.Select onChange={this.props.handleChange} onSubmit={this.props.handleSubmit} aria-label="Default select example">
+          <option>Open this select menu</option>
           <option value="All">All</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="100">100</option>
-          </select>
-        </fieldset>
-        <button type="submit">Submit</button>
-      </form>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          <option value="100">100</option>
+        </Form.Select>
+
+
+        {/* <form onSubmit={this.props.handleSubmit}>
+          <label>Name:
+            <input type="text" name="name" />
+          </label>
+          <fieldset>
+            <legend>Sort by Number of Horns</legend>
+            <select name="selected" onChange={this.props.handleChange}>
+              <option value="All">All</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="100">100</option>
+            </select>
+          </fieldset>
+          <button type="submit">Submit</button>
+        </form> */}
       </>
     );
   };
